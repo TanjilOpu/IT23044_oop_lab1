ae2380ccd4f8356a8b1bf3153a80fe22876d6c5b
Md. Tanjil Islam Opu <tanjilislamopu468@gmail.com>
2024-12-27 12:13:39 +0600

    Update Goldd_.java

--- MARATHON/Goldd_.java
+++ MARATHON/Goldd_.java
@@ -1,29 +1,27 @@
 
 package goldd_;
           // CODEFORCES GOLD RUSH PROBLEM SOLVE WITH JAVA
-
 import java.util.Scanner;
 
 public class Goldd_ {
     static boolean solve(long n, long m) {
         if (n == m) {
-            return true; // Base case: If n equals m, we found the target.
+            return true; 
         }
         if (n < m || n % 3 != 0) {
-            return false; // If n is less than m or n cannot be divided by 3, it's impossible.
+            return false; 
         }
 
         long n1 = n / 3;
         long n2 = 2 * n / 3;
 
-        // Recursively check both splits
+
         return solve(n1, m) || solve(n2, m);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int tc = scanner.nextInt(); // Number of test cases
-
+        int tc = scanner.nextInt();
         while (tc-- > 0) {
             long n = scanner.nextLong();
             long m = scanner.nextLong();
@@ -45,4 +43,5 @@ public class Goldd_ {
         scanner.close();
     }
 }
+
 
